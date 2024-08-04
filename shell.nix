d9529be6859051ec6a592b66525cbe7cc3844c37
Yehoslav Rudenco <yehoslavrudenco@protonmail.com>
2024-08-04 23:21:53 +0300

    added nix shell for build dependancies

---
+++ shell.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> { config.allowUnfree = true; };
+  pkgs.mkShell {
+    buildInputs = [
+      android-studio
+      gradle_6
+      openjdk17-bootstrap 
+      # openjdk8-bootstrap 
+      java-language-server
+
+    ];
+  }
